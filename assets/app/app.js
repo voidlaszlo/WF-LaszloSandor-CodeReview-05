3660c7d12650c7c8feeba33397ac10f06f2bce18
Laszlo Sandor <voidlaszlo@gmail.com>
2019-10-25 08:58:30 +0200

    adding basic structure

---
+++ assets/app/app.js
@@ -0,0 +1,13 @@
+let data = [
+    {
+        "id" : "1",
+        "name" : "Jenny",
+        "age" : "23",
+        "gender" : "female",
+        "location" : "Vienna",
+        "hobbies" : ["skiing", "eating", "music"],
+        "favmusic" : "Enya"
+    }
+]
+
+let containerPeople = document.getElementById('container-people')
